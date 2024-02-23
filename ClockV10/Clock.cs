b2c240f4374ccf180a304779041f5e73f1c22ec9
wawer <wawer@KIWI>
2024-02-23 10:49:29 +0100

    Add project files.

---
+++ ClockV10/Clock.cs
@@ -0,0 +1,71 @@
+﻿namespace ClockV10
+{
+    public class Clock
+    {
+        #region Instance fields
+        private int _hour;
+        private int _minute;
+        #endregion
+
+        #region Constructor
+
+        public Clock(int hour, int minute)
+        {
+            _hour = hour;
+            _minute = minute;
+        }
+        #endregion
+
+        #region Properties
+        public int Hour
+        {
+            get
+            {
+                return _hour;
+            }
+            set
+            {
+                _hour = value; 
+            } 
+
+        }
+        public int Minute
+        {
+            get
+            {
+                return _minute;
+
+            }
+            set
+            {
+                _minute = value;
+
+            }
+
+        }
+        #endregion 
+
+        #region Method
+
+        public bool Over60Minutes()
+        {
+            bool isValid = _minute > 60;
+            return isValid;
+        }
+
+        public void Tick()
+        {
+            Minute = _minute + 1;
+        }
+
+        #endregion
+
+        #region To String
+
+        public override string ToString()
+        {
+            return $"{Hour}:{Minute}";
+        }
+        #endregion
+    }
+}
---
+++ ClockV10/InsertCodeHere.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ClockV10
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+            Clock clock = new Clock(18, 57);
+            Console.WriteLine(clock.ToString());
+
+            clock.Tick();
+
+            Console.WriteLine(clock.ToString());
+
+            clock.Tick();
+            clock.Over60Minutes();
+
+            Console.WriteLine(clock.ToString());
+
+            clock.Tick();
+
+            Console.WriteLine(clock.ToString());
+
+            clock.Tick();
+            clock.Over60Minutes();
+            
+
+            Console.WriteLine(clock.ToString());
+
+            clock.Tick();
+
+            Console.WriteLine(clock.ToString());
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
